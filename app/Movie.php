f94107d761867bf1416e47ff23ba9b87f7bee72e
EddieFreedome <eddie.freed@tutanota.com>
2022-01-26 17:29:28 +0100

    implemented controller logic for taking data from database. Created view displaying data.

--- app/Movie.php
+++ app/Movie.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    //il Model prendera' il suo nome, lo mettera' in minuscolo e al plurale in automatico per "sintonizzarsi" in ascolto del database con lo stesso nome
 }
